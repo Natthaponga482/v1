c6d9f466a2bd6c4eaacfe6c0cd122855377ee342
Natthaponga482 <114330371+Natthaponga482@users.noreply.github.com>
2022-09-25 14:06:02 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showIcon(IconNames.Heart)
+basic.showIcon(IconNames.SmallHeart)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.forever(function () {
+	
+})
